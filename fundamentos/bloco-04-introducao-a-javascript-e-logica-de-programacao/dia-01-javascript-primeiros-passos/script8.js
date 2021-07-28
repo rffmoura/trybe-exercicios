32e30768ead4c1f348a4c17b254dcd1b7d482a5e
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-07-28 19:34:29 -0300

    adiciona oitavo exercicio

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script8.js
@@ -0,0 +1,14 @@
+// 8 - Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
+let a = 9
+let b = 7
+let c = 5
+
+let parImpar = false
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)){
+    parImpar = true
+}
+
+console.log(parImpar)
