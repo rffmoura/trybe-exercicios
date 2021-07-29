3dbd5ce0c3944fec5c1cebf51c87b2ccd01bad62
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-07-29 18:59:56 -0300

    atualiza indentaçao

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script3.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script3.js
@@ -1,6 +1,6 @@
 // 3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
 let soma = 0
 
 for(let i = 0; i < numbers.length; i += 1){
