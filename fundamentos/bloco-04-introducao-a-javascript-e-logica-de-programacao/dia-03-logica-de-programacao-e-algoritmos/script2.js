ceb26cef6bb3f899560719829d2c33279ee87ae4
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-08-05 22:30:18 -0300

    adiciona segundo exericio

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/script2.js
@@ -0,0 +1,10 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+let number = 5
+let symbol = '*'
+let line = ''
+
+for (let i = 0; i <= number; i += 1){
+  console.log(line)
+  line = line + symbol
+}
