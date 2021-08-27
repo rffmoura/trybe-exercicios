8acdfe5200971ee85c7724d2530499f567f623cc
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-08-27 19:24:08 -0300

    adiciona segundo exercicio da parte 1

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -2,7 +2,7 @@
 // Modifique a estrutura da função para que ela seja uma arrow function .
 // Modifique as concatenações para template literals .
 
-const testingScope = escopo => {
+const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = "Não devo ser utilizada fora do meu escopo (if)"
     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`
@@ -11,7 +11,25 @@ const testingScope = escopo => {
     let elseScope = "Não devo ser utilizada fora meu escopo (else)"
     console.log(elseScope)
   }
-  // console.log(ifScope + " o que estou fazendo aqui ? :O") // Se necessário esta linha pode ser removida.
+  // console.log(ifScope + " o que estou fazendo aqui ? :O")
 }
 
 testingScope(true)
+
+// 2 - Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2]
+
+const sorting = () => {
+  oddsAndEvens[0] = 2
+  oddsAndEvens[1] = 3
+  oddsAndEvens[2] = 4
+  oddsAndEvens[3] = 7
+  oddsAndEvens[4] = 10
+  oddsAndEvens[5] = 13
+
+  return oddsAndEvens
+}
+
+console.log(`Os números ${sorting()} se encontram na ordem crescente!`)
