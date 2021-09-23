a9cd8e471c02fb757d924acf2e0aa6922a7d076c
Maitê Caetano <maitecaetano@MacBook-Air-de-Maite.local>
2021-09-23 17:27:28 -0300

    adding styles

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Loading extends React.Component {
-    render(){
-        return(
-            <div className='loading'>
-                Carregando...
-            </div>
-        )
-    }
+  render(){
+    return(
+      <div className='loading'>
+          <h1>Carregando...</h1>
+      </div>
+    )
+  }
 }
 
 export default Loading;
