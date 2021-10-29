b9d289de051284e8b4568b8f3517ab3f980ea2c2
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-10-29 20:08:48 -0300

    adiciona exercicios do dia

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderiza a palavra "carregando" na tela', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const carregando = screen.getByText(/carregando/i)
+  expect(carregando).toBeInTheDocument()
 });
+
+test('Verifica se o jogo Minecraft aparece tela', async () => {
+  render(<App />);
+  
+  await waitForElementToBeRemoved(() => screen.getByText(/carregando/i))
+  
+  const minecraft = await screen.getAllByText(/minecraft/i)
+  expect(minecraft[0]).toBeInTheDocument()
+})
+
+test('verifica detalhes', async () => {
+  render(<App />)
+
+  const detalhes = await screen.findAllByText(/ver detalhes/i)
+  expect(detalhes[1]).toBeInTheDocument()
+
+  userEvent.click(detalhes[1]);
+
+  const releaseYear = await screen.findByText(/release year/i)
+  expect(releaseYear).toBeInTheDocument()
+
+  const sales = await screen.findByText(/sales/i)
+  expect(sales).toBeInTheDocument()
+})
+
+test('botão', async () => {
+  render(<App />)
+
+  await waitForElementToBeRemoved(() => screen.getByText(/carregando/i))
+
+  const btn = screen.getByText(/voltar/i)
+  userEvent.click(btn)
+
+  await waitForElementToBeRemoved(() => screen.getByText(/carregando/i))
+
+  const games = screen.getByRole('heading', {name: /os melhores jogos/i})
+  expect(games).toBeInTheDocument()
+})
